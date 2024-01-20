0635fd2391fee87504a1f4dd3e1ec4e1edb47b90
Attila Olbrich <olbrichattila@gmx.co.uk>
2024-01-20 22:40:43 +0000

    Some fixes

--- frontend/src/pages/sqs.js
+++ frontend/src/pages/sqs.js
@@ -6,73 +6,72 @@ import LoadingSpinner from '../icons/loadingSpinner';
 import SaveBox from '../components/savebox';
 
 const SqsPage = () => {
-  const [data, setData] = useState([]);
-  const [watch, setWatch] = useState(-1);
+    const [data, setData] = useState([]);
+    const [watch, setWatch] = useState(-1);
 
-  const refreshQueue = (attributes, idx) => {
-    setData(
-      [...data.slice(0, idx), {...data[idx], attributes}, ...data.slice(idx + 1)]
+    const refreshQueue = (attributes, idx) => {
+        setData(
+            [...data.slice(0, idx), { ...data[idx], attributes }, ...data.slice(idx + 1)]
+        );
+    }
+
+    useEffect(() => {
+        const updateTimer = () => {
+            if (watch >= 0) {
+                refresh(data[watch].url).then(r => refreshQueue(r, watch))
+            }
+        };
+
+        const timerId = setInterval(updateTimer, 2000);
+
+        return () => {
+            clearInterval(timerId);
+
+        };
+    }, [watch]);
+
+    useEffect(() => {
+        load().then(r => setData(r));
+    }, []);
+
+    return (
+        <div>
+            <SaveBox
+                title='New SQS queue name:'
+                onSubmit={queueName => save(queueName).then(() => load().then(r => setData(r)))}
+            />
+            <table className='sqsTable'>
+                <thead>
+                    <tr>
+                        <th>Queue Name</th>
+                        <th className="narrow">Messages</th>
+                        <th className="narrow">Messages NotVisible</th>
+                        <th className="narrow">Messages Delayed</th>
+                        <th colSpan={4}></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, idx) => (
+                        <tr className="sqsRow" key={idx}>
+                            <td>{item.url.split('/').pop()}</td>
+                            <td>{item.attributes.ApproximateNumberOfMessages}</td>
+                            <td>{item.attributes.ApproximateNumberOfMessagesNotVisible}</td>
+                            <td>{item.attributes.ApproximateNumberOfMessagesDelayed}</td>
+                            <td className="narrow">{<Button onClick={() => {
+                                del(data[idx].url).then(() => load().then(r => setData(r)));
+                                setWatch(-1);
+                            }} label="Delete" />}</td>
+                            <td className="narrow">{<Button onClick={() => purge(data[idx].url).then(() => load().then(r => setData(r)))} label="Purge" />}</td>
+                            <td className="narrow">{<Button onClick={() => refresh(data[idx].url).then(r => refreshQueue(r, idx))} label="Refresh" />}</td>
+                            <td className="narrow">
+                                {watch !== idx ? <Button label="Watch" onClick={() => setWatch(idx)} /> : <LoadingSpinner onClick={() => setWatch(-1)} />}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
-  }
-
-  useEffect(() => {
-    const updateTimer = () => {
-      if (watch >= 0) {
-        refresh(data[watch].url).then(r => refreshQueue(r, watch))
-      }
-    };
-
-    const timerId = setInterval(updateTimer, 2000);
-
-    return () => {
-      clearInterval(timerId);
-      
-    };
-  }, [watch]);
-
-  useEffect(() => {
-    load().then(r => setData(r));
-  }, []);
-
-  return (
-    <div>
-      <SaveBox
-        title='New SQS queue name:'
-        onSubmit={queueName => save(queueName).then(() => load().then(r => setData(r)))}
-      />
-      <h1>Data from JSON API</h1>
-      <table className='sqsTable'>
-        <thead>
-          <tr>
-            <th>Queue Name</th>
-            <th className="narrow">Messages</th>
-            <th className="narrow">Messages NotVisible</th>
-            <th className="narrow">Messages Delayed</th>
-            <th className="narrow">Delete</th>
-            <th className="narrow">Purge</th>
-            <th className="narrow">Refresh</th>
-            <th className="narrow"></th>
-          </tr>
-        </thead>
-        <tbody>
-        {data.map((item, idx) => (
-          <tr className="sqsRow" key={idx}>
-            <td>{item.url.split('/').pop()}</td>
-            <td>{item.attributes.ApproximateNumberOfMessages}</td>
-            <td>{item.attributes.ApproximateNumberOfMessagesNotVisible}</td>
-            <td>{item.attributes.ApproximateNumberOfMessagesDelayed}</td>
-            <td>{<Button onClick={() => del(data[idx].url).then(() => load().then(r => setData(r)))} label="Delete" />}</td>
-            <td>{<Button onClick={() => purge(data[idx].url).then(() => load().then(r => setData(r)))} label="Purge" />}</td>
-            <td>{<Button onClick={() => refresh(data[idx].url).then(r => refreshQueue(r, idx))} label="Refresh" />}</td>
-            <td>
-              {watch !== idx ? <Button label="Watch" onClick={() => setWatch(idx)}/> : <LoadingSpinner onClick={() => setWatch(-1)} />}
-            </td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
-    </div>
-  );
 };
 
 export default SqsPage;
