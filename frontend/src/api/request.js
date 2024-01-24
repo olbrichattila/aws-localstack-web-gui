59a3142cb601900118712efb226396eb8e1100a7
Attila Olbrich <olbrichattila@gmx.co.uk>
2024-01-24 20:48:05 +0000

    Comment line back

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -32,7 +32,7 @@ const request = async (type, path, payload = null) => {
         return data;
     } catch (error) {
         console.error('Error fetching data:', error.message);
-        // throw error;
+        throw error;
         // return [];
 
     }
